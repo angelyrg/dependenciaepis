f0cfdc9490005c025857d9f3d6833e017a5b05e7
angelyrg <angel.yarang4@gmail.com>
2022-07-20 06:05:42 -0500

    Crear usuario al registra docente

--- app/Http/Controllers/DocenteController.php
+++ app/Http/Controllers/DocenteController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\DocenteRequest;
 use App\Models\Docente;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class DocenteController extends Controller
 {
@@ -28,6 +30,14 @@ class DocenteController extends Controller
     public function store(DocenteRequest $request)
     {
         Docente::create($request->all());
+
+        $user = new User();
+        $user->name = $request->nombres." ".$request->apellidos;
+        $user->username = $request->dni;
+        $user->password = Hash::make($request->dni);
+        $user->rol = 'Asesor';
+        $user->save();
+
         return redirect()->route('docentes.index')->with('success', 'Docente '.$request->nombres.' registrado correctamente.');
     }
 
